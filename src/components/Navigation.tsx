33fcc1bbec55b1b107aba6284861d05ab9c0cf90
ayberkuman <ayberkuman1@gmail.com>
2022-08-31 15:59:45 +0300

    header fix

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -8,13 +8,13 @@ export const Navigation = ({ username }: NavigationProps) => {
       <nav className="lg:flex lg:gap-12 md:grid md:grid-cols-2 md:gap-5 t-14 font-lato hidden font-medium">
         <a className="cursor-pointer">PRODUCTS</a>
         <a className="cursor-pointer">BRANDS</a>
-        <div className="flex">
+        <div className="flex items-center">
           <ProfilePicPlaceHolder />
-          <a className="cursor-pointer">HELLO, {username.toUpperCase()}</a>
+          <a className="cursor-pointer px-2">HELLO, {username.toUpperCase()}</a>
         </div>
-        <div className="flex">
+        <div className="flex items-center">
           <CartIcon />
-          <a className="cursor-pointer">CART</a>
+          <a className="cursor-pointer px-2">CART</a>
         </div>
       </nav>
       <button className="md:hidden">
